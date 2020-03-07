a481b2acd575cd5ae8dc1dd01b4dabb45949c0f5
Charles Cusson <charlescusson@gmail.com>
2020-03-07 20:45:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -61,11 +61,10 @@ if (age >=13 && <=19){
 }
 
 function teenager(age) {
-  if (age >=13 && <=19){
-   return "You are a teenager!";}
-  else
-   return {"You are not a teenager";}
- }
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else {return "You are not a teenager";}
+}
 
 
 function ageChecker(age) {
